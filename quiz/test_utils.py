fda6777568176b2df51f390f3bcceffc1f081e12
Tomasz Staszkiewicz <tomasz.staszkiewicz@plumresearch.com>
2023-03-06 13:19:21 +0100

    add more util unit tests

--- quiz/test_utils.py
+++ quiz/test_utils.py
@@ -23,9 +23,17 @@ from quiz.utils import (
     set_initial_score_data,
     SENIORITY_CHOICES,
     save_number_of_finished_series,
+    template_choice,
+    check_question_type_to_update_score,
+    update_score,
 )
 
-from quiz.fixture_factories import ScoreDictFactory, SCORE_DATA
+from quiz.fixture_factories import (
+    ScoreDictFactory,
+    SCORE_DATA,
+    AnswerDictFactory,
+    AuthorDictFactory,
+)
 
 
 class TestUtils:
@@ -62,6 +70,25 @@ class TestUtils:
         self.score_data["technology"] = self.technology
 
         self.score = Score.objects.create(**self.score_data)
+        self.score.score_data["general_score"] = 0
+        self.score.score_data["junior_score"] = 0
+        self.score.score_data["regular_score"] = 0
+        self.score.score_data["senior_score"] = 0
+        self.score.score_data["seniority_level"] = 1
+
+        self.answer_data = AnswerDictFactory.build()
+        self.author_data = AuthorDictFactory.build()
+        self.question_data = self.answer_data["question"]
+        self.author = Author.objects.create(**self.author_data)
+
+        self.question_data["category"] = self.category
+        self.question_data["technology"] = self.technology
+        self.question_data["seniority"] = self.seniority
+        self.question_data["author"] = self.author
+        self.question = Question.objects.create(**self.question_data)
+
+        self.answer_data["question"] = self.question
+        self.answer = [Answer.objects.create(**self.answer_data)]
 
     def test_if_prepare_technologies_in_session__adds_technologies_to_session(self):
         session = self.request.session
@@ -115,3 +142,192 @@ class TestUtils:
         assert score.score_data["number_of_junior_series"] == 1
         assert score.score_data["number_of_regular_series"] == 1
         assert score.score_data["number_of_senior_series"] == 1
+
+    def test_template_choice(self):
+        question_types = ("open", "multiple choice", "true/false")
+        expected_results = (
+            "single_question_open.html",
+            "single_question.html",
+            "boolean_question.html",
+        )
+        for question_type, expected_result in zip(question_types, expected_results):
+            template = template_choice(question_type)
+            assert template == expected_result
+
+    def test_update_score(self):
+        score = copy.deepcopy(self.score)
+        score = update_score(score)
+
+        assert score.score_data["general_score"] == 1
+        assert score.score_data["junior_score"] == 1
+        assert score.score_data["regular_score"] == 0
+        assert score.score_data["senior_score"] == 0
+
+        score.score_data["seniority_level"] = 2
+        score = update_score(score)
+
+        assert score.score_data["general_score"] == 2
+        assert score.score_data["junior_score"] == 1
+        assert score.score_data["regular_score"] == 1
+        assert score.score_data["senior_score"] == 0
+
+        score.score_data["seniority_level"] = 3
+        score = update_score(score)
+
+        assert score.score_data["general_score"] == 3
+        assert score.score_data["junior_score"] == 1
+        assert score.score_data["regular_score"] == 1
+        assert score.score_data["senior_score"] == 1
+
+    def test_check_question_type_to_update_score__changes_score_when_question_is_boolean_type(
+        self,
+    ):
+        self.question.question_type = "true/false"
+        post_request = RequestFactory().post(
+            path=self.url,
+            data={"ans": self.answer[0].text, "csrfmiddlewaretoken": "xxx"},
+        )
+        score = copy.deepcopy(self.score)
+        score = check_question_type_to_update_score(
+            post_request, self.question, self.answer, score
+        )
+
+        assert score.score_data["general_score"] == 1
+        assert score.score_data["junior_score"] == 1
+        assert score.score_data["regular_score"] == 0
+        assert score.score_data["senior_score"] == 0
+
+        score.score_data["seniority_level"] = 2
+        score = check_question_type_to_update_score(
+            post_request, self.question, self.answer, score
+        )
+
+        assert score.score_data["general_score"] == 2
+        assert score.score_data["junior_score"] == 1
+        assert score.score_data["regular_score"] == 1
+        assert score.score_data["senior_score"] == 0
+
+        score.score_data["seniority_level"] = 3
+        score = check_question_type_to_update_score(
+            post_request, self.question, self.answer, score
+        )
+
+        assert score.score_data["general_score"] == 3
+        assert score.score_data["junior_score"] == 1
+        assert score.score_data["regular_score"] == 1
+        assert score.score_data["senior_score"] == 1
+
+    def test_check_question_type_to_update_score__changes_score_when_question_is_open_type(
+        self,
+    ):
+        self.question.question_type = "open"
+        post_request = RequestFactory().post(
+            path=self.url,
+            data={"ans": self.answer[0].text, "csrfmiddlewaretoken": "xxx"},
+        )
+        score = copy.deepcopy(self.score)
+        score = check_question_type_to_update_score(
+            post_request, self.question, self.answer, score
+        )
+
+        assert score.score_data["general_score"] == 1
+        assert score.score_data["junior_score"] == 1
+        assert score.score_data["regular_score"] == 0
+        assert score.score_data["senior_score"] == 0
+
+        score.score_data["seniority_level"] = 2
+        score = check_question_type_to_update_score(
+            post_request, self.question, self.answer, score
+        )
+
+        assert score.score_data["general_score"] == 2
+        assert score.score_data["junior_score"] == 1
+        assert score.score_data["regular_score"] == 1
+        assert score.score_data["senior_score"] == 0
+
+        score.score_data["seniority_level"] = 3
+        score = check_question_type_to_update_score(
+            post_request, self.question, self.answer, score
+        )
+
+        assert score.score_data["general_score"] == 3
+        assert score.score_data["junior_score"] == 1
+        assert score.score_data["regular_score"] == 1
+        assert score.score_data["senior_score"] == 1
+
+    def test_check_question_type_to_update_score__changes_score_when_question_is_multichoice_type(
+        self,
+    ):
+        self.question.question_type = "multiple choice"
+        self.answer[0].is_correct = True
+        post_request = RequestFactory().post(
+            path=self.url,
+            data={
+                f"{self.answer[0].pk}": self.answer[0].text,
+                "csrfmiddlewaretoken": "xxx",
+            },
+        )
+        score = copy.deepcopy(self.score)
+        score = check_question_type_to_update_score(
+            post_request, self.question, self.answer, score
+        )
+
+        assert score.score_data["general_score"] == 1
+        assert score.score_data["junior_score"] == 1
+        assert score.score_data["regular_score"] == 0
+        assert score.score_data["senior_score"] == 0
+
+        score.score_data["seniority_level"] = 2
+        score = check_question_type_to_update_score(
+            post_request, self.question, self.answer, score
+        )
+
+        assert score.score_data["general_score"] == 2
+        assert score.score_data["junior_score"] == 1
+        assert score.score_data["regular_score"] == 1
+        assert score.score_data["senior_score"] == 0
+
+        score.score_data["seniority_level"] = 3
+        score = check_question_type_to_update_score(
+            post_request, self.question, self.answer, score
+        )
+
+        assert score.score_data["general_score"] == 3
+        assert score.score_data["junior_score"] == 1
+        assert score.score_data["regular_score"] == 1
+        assert score.score_data["senior_score"] == 1
+
+    def test_check_question_type_to_update_score__not_changes_score_when_answer_is_wrong(
+        self,
+    ):
+        self.question.question_type = "true/false"
+        post_request = RequestFactory().post(
+            path=self.url, data={"ans": "wrong answer", "csrfmiddlewaretoken": "xxx"}
+        )
+        score = copy.deepcopy(self.score)
+        score = check_question_type_to_update_score(
+            post_request, self.question, self.answer, score
+        )
+
+        assert score.score_data["general_score"] == 0
+        assert score.score_data["junior_score"] == 0
+        assert score.score_data["regular_score"] == 0
+        assert score.score_data["senior_score"] == 0
+
+    def test_check_question_type_to_update_score__not_changes_score_when_answer_is_wrong_and_question_is_multichoice_type(
+        self,
+    ):
+        self.question.question_type = "multiple choice"
+        post_request = RequestFactory().post(
+            path=self.url,
+            data={f"wrong pk": "wrong answer", "csrfmiddlewaretoken": "xxx"},
+        )
+        score = copy.deepcopy(self.score)
+        score = check_question_type_to_update_score(
+            post_request, self.question, self.answer, score
+        )
+
+        assert score.score_data["general_score"] == 0
+        assert score.score_data["junior_score"] == 0
+        assert score.score_data["regular_score"] == 0
+        assert score.score_data["senior_score"] == 0
